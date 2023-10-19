af0ae885b90abb6893a5537917e52f0cb5e5a89d
Elias <elias.souza.es609@gmail.com>
2023-10-19 20:17:07 -0300

    adicionado metodo para atualizar as notas

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -5,7 +5,7 @@ const withAuth = require('../middleware/auth'); // Verifique o caminho correto
 
 router.post('/', withAuth, async (req, res) => {
     const { title, body } = req.body;
-    
+
     try {
         let note = new Note({ title: title, body: body, author: req.user._id });
         await note.save();
@@ -18,7 +18,7 @@ router.post('/', withAuth, async (req, res) => {
 
 router.get('/', withAuth, async (req, res) => {
     try {
-        let notes = await Note.find({author: req.user._id})
+        let notes = await Note.find({ author: req.user._id })
         res.json(notes);
     } catch (error) {
         res.status(500).json({ error: "Problema to get a note" });
@@ -36,6 +36,29 @@ router.get('/:id', withAuth, async (req, res) => {
         }
     } catch (error) {
         res.status(500).json({ error: "Problema to get a note" });
+    }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    const { title, body } = req.body;
+    const { id } = req.params;
+
+    try {
+        let note = await Note.findById(id);
+        if (isOwner(req.user, note)) {
+            let updatedNote = await Note.findOneAndUpdate(
+                { _id: id },
+                { $set: { title , body } },
+                { upsert: true, 'new' : true }
+            );
+            res.json(note)
+        } else {
+            res.status(403).json({ error: "Not permission for update this Note" });
+        }
+
+    } catch (error) {
+        console.error(err)
+        res.status(500).json({ error: "Problema to update a note" });
     }
 });
 
