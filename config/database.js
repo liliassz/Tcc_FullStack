3e78c4b8e4eb33813688ad9c1fd6a121a750a2d8
Elias Souza <elias.souza.es609@gmail.com>
2023-10-24 11:47:11 -0300

    update

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 mongoose.connect('mongodb://127.0.0.1:27017/tcc', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true
 }).then(() => console.log('Conexão com o banco de dados Estabelecida'))
   .catch((err) => console.log(err))
 
