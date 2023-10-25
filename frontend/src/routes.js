0a17e1fa483fee0a8f03b99dbcefef7b5bbaacc2
Elias Souza <elias.souza.es609@gmail.com>
2023-10-25 10:51:38 -0300

    Routes Added

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importe o BrowserRouter e Routes
 import HomeScreen from './screens/home'
+import LoginScreen from './screens/auth/login'
+import RegisterScreen from './screens/auth/register'
+import UserEditScreen from './screens/users/edit'
+import NotesScreen from './screens/notes/index'
 
 const Rotas = () => (
     <Router>
         <Routes>
-            <Route path='*' element={<HomeScreen />}/>
+            <Route exact path='*' element={<HomeScreen />}/>
+            <Route exact path='/login' element={<LoginScreen />}/>
+            <Route exact path='/register' element={<RegisterScreen />}/>
+            <Route exact path='/users/edit' element={<UserEditScreen />}/>
+            <Route exact path='/notes' element={<NotesScreen />}/>
         </Routes>
     </Router>
 );
